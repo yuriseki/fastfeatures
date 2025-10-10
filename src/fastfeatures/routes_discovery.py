8e2cff9a3eae133a62d9670a68da8ee0a74a87d2
Yuri Seki <yuriseki@gmail.com>
2025-10-09 22:34:37 -0300

    Initial commit.

---
+++ src/fastfeatures/routes_discovery.py
@@ -0,0 +1,10 @@
+"""
+Docstring for fast_features.routes_discovery
+"""
+def discovery_routes() -> list[str]:
+    """Discovery features routes.
+
+    Returns:
+        list[str]: _description_
+    """
+    return ["This is a placeholder for route discovery."]
